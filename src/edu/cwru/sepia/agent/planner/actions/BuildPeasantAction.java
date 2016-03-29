71a246b85641b49141fdecd8cb3cbb0bfe8b031a
James <jrf116@case.edu>
2016-03-29 19:20:34 -0400

    Added BuildPeasantAction.

---
+++ src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
@@ -0,0 +1,40 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.Peasant;
+import edu.cwru.sepia.agent.planner.Position;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by jamesflinn on 3/29/16.
+ */
+public class BuildPeasantAction implements StripsAction {
+    /**
+     * Preconditions are met if the player has at least 400 gold,
+     * and if there are less than 3 peasants already in existance.
+     * @param state GameState to check if action is applicable
+     * @return true if preconditions are met
+     */
+    @Override
+    public boolean preconditionsMet(GameState state) {
+        return state.getCurrentGold() >= 400 && state.getPeasantsMap().size() < 3;
+    }
+
+    @Override
+    public GameState apply(GameState state) {
+        int newID = state.getPeasantsMap().size() + 1;
+        Position newPosition = new Position(state.getTownhall().x + 1, state.getTownhall().y);
+        Peasant newPeasant = new Peasant(newID, newPosition);
+
+        Map<Integer, Peasant> newPeasantMap = new HashMap<>(state.getPeasantsMap());
+        newPeasantMap.put(newID, newPeasant);
+
+        Stack<StripsAction> actions = (Stack<StripsAction>) state.getPreviousActions().clone();
+        actions.push(this);
+
+        return new GameState(state, state.getGoldLocations(), state.getTreeLocations(), newPeasantMap, state.getCurrentGold(), state.getCurrentWood(), actions);
+    }
+}
